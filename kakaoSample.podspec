b24e12525bc1d2d1c61df606d415802fd1e54681
pete <iwwuf7@gmail.com>
2020-09-14 10:03:51 +0900

    0.1.6

--- kakaoSample.podspec
+++ kakaoSample.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'kakaoSample'
-  s.version          = '0.1.5'
+  s.version          = '0.1.6'
   s.summary          = 'just sample'
 
   s.swift_versions = '4.2'
---
+++ kakaoSample/kakaoSample.h
@@ -0,0 +1,19 @@
+//
+//  kakaoSample.h
+//  kakaoSample
+//
+//  Created by jinho jang on 2020/09/14.
+//  Copyright © 2020 People. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kakaoSample.
+FOUNDATION_EXPORT double kakaoSampleVersionNumber;
+
+//! Project version string for kakaoSample.
+FOUNDATION_EXPORT const unsigned char kakaoSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kakaoSample/PublicHeader.h>
+
+
